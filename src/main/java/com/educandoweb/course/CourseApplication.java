e61eca4c20caa138e85bfb1a15454d13086eb5e6
thiago.leite <thiago.leite@dataprev.gov.br>
2023-06-20 09:26:32 -0300

    Porta configurada e processo desligado pra acessar o porta 8080

--- src/main/java/com/educandoweb/course/CourseApplication.java
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CourseApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("server.port", "8080");
 		SpringApplication.run(CourseApplication.class, args);
 	}
 
