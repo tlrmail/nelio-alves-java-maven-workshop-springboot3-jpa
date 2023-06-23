ce40ae5f7a88054e94f6b28f26fba1222d573159
thiago.leite <thiago.leite@dataprev.gov.br>
2023-06-23 11:49:45 -0300

    200~Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/config/TesteConfig.java
+++ src/main/java/com/educandoweb/course/config/TesteConfig.java
@@ -51,6 +51,16 @@ public class TesteConfig implements CommandLineRunner{
 		
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
+		p1.addCategory(cat2);
+		p2.addCategory(cat1);
+		p2.addCategory(cat3);
+		p3.addCategory(cat3);
+		p4.addCategory(cat3);
+		p5.addCategory(cat2);
+		
+		//Salva os produtos relacionados as categorias
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 
